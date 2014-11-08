aeaa3d760b513253fb1fa7c9f7023376e1905867
Johannes Wolf <johannes@n3rdkeller.de>
2014-11-08 21:31:26 +0100

    Init commit & add first iteration of mvkey script

---
+++ mvkey.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo 'This command is not compatible with any scp flags. Set up the ssh connection in your ssh config first.'
+echo ''
+decider=false
+if [ "${1##*.}" != "pub" ]; then
+ read -p "$1 doesn't have a .pub ending and may not be a public key. Do you want to continue? [Y/n] " yn
+ case $yn in
+   [Yy]* ) decider=true;;
+   [Nn]* ) ;;
+       * ) decider=true;;
+ esac
+else
+ decider=true
+fi
+if [ scp $1 $2 ] && [ $decider == true ]; then
+  rm $1
+  read -p "Do you want to directly append the key to the remote authorized_keys? [y/N]" yn
+  case $yn in
+      [Yy]* ) ssh $(echo $2 | cut -d ':' -f1) -t $(echo 'cd ')$(echo $(echo $2 | cut -d ':' -f2))$(echo '; cat ')$(echo $1)$(echo '>> authorized_keys; rm ')$(echo $1);;
+      [Nn]* ) echo $(echo 'connecting to ')$(echo $2)
+              ssh $(echo $2 | cut -d ':' -f1) -t $(echo 'cd ')$(echo $(echo $2 | cut -d ':' -f2))$(echo '; $SHELL -l');;
+          * ) echo $(echo 'connecting to ')$(echo $2)
+              ssh $(echo $2 | cut -d ':' -f1) -t $(echo 'cd ')$(echo $(echo $2 | cut -d ':' -f2))$(echo '; $SHELL -l');;
+  esac
+fi
