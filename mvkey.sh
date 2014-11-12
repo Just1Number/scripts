796bebf575e8cda8ae226da5ac1b4e8949caf8be
Johannes Wolf <johannes@n3rdkeller.de>
2014-11-12 02:14:27 +0100

    Small change

--- mvkey.sh
+++ mvkey.sh
@@ -12,12 +12,13 @@ if [ "${1##*.}" != "pub" ]; then
 else
  decider=true
 fi
-if [ scp $1 $2 ] && [ $decider == true ]; then
+if [ $decider == true ] && [ $(scp $1 $2) ]; then
   rm $1
   read -p "Do you want to directly append the key to the remote authorized_keys? [y/N]" yn
   case $yn in
-      [Yy]* ) ssh $(echo $2 | cut -d ':' -f1) -t $(echo 'cd ')$(echo $(echo $2 | cut -d ':' -f2))$(echo '; cat ')$(echo $1)$(echo '>> authorized_keys; rm ')$(echo $1)
-              echo "appended $1 to remote authorized_keys";;
+      [Yy]* ) if [ ssh $(echo $2 | cut -d ':' -f1) -t $(echo 'cd ')$(echo $(echo $2 | cut -d ':' -f2))$(echo '; cat ')$(echo $1)$(echo '>> authorized_keys; rm ')$(echo $1) ]; then
+                echo "appended $1 to remote authorized_keys"
+              fi;;
           * ) echo $(echo 'connecting to ')$(echo $2)
               ssh $(echo $2 | cut -d ':' -f1) -t $(echo 'cd ')$(echo $(echo $2 | cut -d ':' -f2))$(echo '; $SHELL -l');;
   esac
