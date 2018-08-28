ce0ff94c75d8b2d1d90551bf8f04f0d19002ef5b
Johannes Wolf <johannes@wolfs.de>
2018-08-28 16:59:45 +0200

    Improve dscovr.py
    To pick an image the script now compares the time of day the images were taken, to the current time of day.

--- dscovr.py
+++ dscovr.py
@@ -12,12 +12,41 @@ api_url = "https://epic.gsfc.nasa.gov/api/natural"
 image_source = "https://epic.gsfc.nasa.gov/archive/natural/"
 
 # Parsing api 
-contents = urlopen(api_url).read()
+contents = bytes(0)
+try:
+    contents = urlopen(api_url).read()
+except:
+    print("Problem with the api url " + api_url)
+    print("Check your internet connection")
+    exit()
 data = json.loads(contents.decode('utf-8'))
 opener = URLopener()
 
+image_times = []
+tsnow = datetime.utcnow().timestamp()
+
+for meta in data:
+    # convert json date into python time object, ignoring the date
+    t = datetime.strptime(meta['date'], "%Y-%m-%d %H:%M:%S").time()
+    # set date to today
+    dt = datetime.combine(datetime.fromtimestamp(tsnow).date(), t)
+    # return to timestamp
+    ts = dt.timestamp()
+    image_times.append(ts)
+
+# handle the special case of tsnow being in between the last and the first image of the day
+if tsnow < image_times[0]:
+    image_times[len(image_times) - 1] -= 24 * 3600
+elif tsnow > image_times[len(image_times) - 1]:
+    image_times[0] += 24*3600
+
+diffs = []
+for ts in image_times:
+    diff = abs( tsnow - ts )
+    diffs.append(diff)
+
 # choosing image by index. picking a picture taken close to the current daytime
-index = int(datetime.now().hour/ 24.0 * len(data)) # dates.index(max(dates))
+index = diffs.index(min(diffs))
 image_name = data[index]['image'] + '.png' # epic_1b_20180630224431.png
 image_id = data[index]['identifier'] # 20180630224431
 image_path = download_directory + image_name
